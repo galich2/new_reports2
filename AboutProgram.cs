1fec643b8e7ac11cb2a21ba83735e2e0f4e10966
galich2 <galich2@mail.ru>
2019-11-11 13:49:10 +0400

    11112019

--- AboutProgram.cs
+++ AboutProgram.cs
@@ -101,5 +101,10 @@ namespace Reports
             }
         }
         #endregion
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
